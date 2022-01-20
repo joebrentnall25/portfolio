d09a0f5c5884df17004fe791f83ffff5a82c580a
Joe Brentnall <joebrentnall@Joes-MBP.lan>
2022-01-20 16:17:50 +0000

    Added About page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.scss';
 import Intro from "./Containers/Intro/Intro";
+import About from './Containers/About/About';
 
 function App() {
   return (
     <div className="App">
       <Intro/>
+      <About/>
     </div>
   );
 }
---
+++ src/Containers/About/About.jsx
@@ -0,0 +1,12 @@
+import "./About.scss";
+
+const About = () => {
+    return (
+        <section className="about">
+            <h3 className="about__header">About Me.</h3>
+            <p className="about__para">Im Joe and I'm Junior Full Stack Developer based in the East Midlands. I enjoy both front end and backend development. And hava a keen interest in developing APIs and working with Cloud technologies. I hope to expand on this with any future opportunities.</p>
+        </section>
+    )
+}
+
+export default About;
