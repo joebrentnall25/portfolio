57cf10f6f22990761b6b35f4dc36e2d06b86691f
Joe Brentnall <joebrentnall@Joes-MBP.lan>
2022-01-20 16:04:42 +0000

    Added boiler plate and added intro to portfolio

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss';
+import Intro from "./Containers/Intro/Intro";
+
+function App() {
+  return (
+    <div className="App">
+      <Intro/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Containers/Intro/Intro.jsx
@@ -0,0 +1,11 @@
+import "./Intro.scss";
+
+const Intro = () => {
+    return (
+        <section className="intro">
+            <h2 className="intro__heading">Hello, I'm <span className="intro__heading-span">Joe Brentnall</span>. Welcome to my portfolio!</h2>
+        </section>
+    )
+}
+
+export default Intro;
